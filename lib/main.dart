6c3be8f3565362b3045c178f8857e435d994d80a
V4L3NT1N3 <33112835+V4L3NT1N3@users.noreply.github.com>
2021-02-11 14:21:46 +0800

    There isn't anything new

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -27,13 +26,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   String temp = '';
   String address = '123456789';
   var _controller = TextEditingController();
   var _controller1 = TextEditingController();
   final _formkey = GlobalKey<FormState>();
-
 
   Uint8List _imageFile;
   ScreenshotController screenshotController = ScreenshotController();
@@ -43,60 +40,47 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       body: Row(
         children: [
-          Column(
-              children: [
-                Text("Logo"),
-                Text("Logo"),
-                Padding(
-                  padding: EdgeInsets.fromLTRB(20, 20, 0, 0),
-                  child: Container(
-                    height: 200,
-                    width: 150,
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.red,
-                      ),
-
-                    ),
-                    child: Column(
-                      children: [
-                        TextFormField(
-                          controller: _controller,
-                          onChanged: (text) {
-                            setState(() {
-                              address = text;
-                            });
-                          },
-                        ),
-                        TextFormField(
-                          controller: _controller,
-                          onChanged: (text) {
-                            setState(() {
-                              address = text;
-                            });
-                          },
-                        ),
-                        TextButton(onPressed: (){
-
-                        }
-                        , child: Text('Submit')),
-
-
-                      ],
-                    ),
+          Column(children: [
+            Text("Logo"),
+            Text("Logo"),
+            Padding(
+              padding: EdgeInsets.fromLTRB(20, 20, 0, 0),
+              child: Container(
+                height: 200,
+                width: 150,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.red,
                   ),
                 ),
-
-              ]
-          ),
-          SizedBox(
-            width: 1100,
-          ),
+                child: Column(
+                  children: [
+                    TextFormField(
+                      controller: _controller,
+                      onChanged: (text) {
+                        setState(() {
+                          address = text;
+                        });
+                      },
+                    ),
+                    TextFormField(
+                      controller: _controller,
+                      onChanged: (text) {
+                        setState(() {
+                          address = text;
+                        });
+                      },
+                    ),
+                    TextButton(onPressed: () {}, child: Text('Submit')),
+                  ],
+                ),
+              ),
+            ),
+          ]),
+          Spacer(),
           Column(
             children: [
-              Align(
-                  alignment: Alignment.topRight,
-                  child: Text("About Us")),
+              Align(alignment: Alignment.topRight, child: Text("About Us")),
               Screenshot(
                 controller: screenshotController,
                 child: QrImage(
@@ -105,31 +89,31 @@ class _MyHomePageState extends State<MyHomePage> {
                   size: 200.0,
                 ),
               ),
-              TextButton(onPressed: () {
-                _imageFile = null;
-                screenshotController
-                    .capture()
-                    .then((Uint8List image) async {
-                  setState(() {
-                    _imageFile = image;
-                  });
+              TextButton(
+                onPressed: () {
+                  _imageFile = null;
+                  screenshotController.capture().then((Uint8List image) async {
+                    setState(() {
+                      _imageFile = image;
+                    });
 
-                  print("File Saved to Gallery");
-                }).catchError((onError) {
-                  print(onError);
-                });
-              }, child: Text("Save Image"),),
+                    print("File Saved to Gallery");
+                  }).catchError((onError) {
+                    print(onError);
+                  });
+                },
+                child: Text("Save Image"),
+              ),
+              TextButton(
+                  child: Text('Download Image'),
+                  onPressed: () {
+                    var _image = MemoryImage(_imageFile);
+                    // TODO implement JavaScript to download image
+                  })
             ],
           ),
-
         ],
-
       ),
     );
   }
 }
-
-
-
-
-
